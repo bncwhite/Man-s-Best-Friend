ee7f14049d718d1a85611ba562353149c7c5c491
bncwhite <codebybrad@icloud.com>
2014-08-14 11:43:39 -0500

    Initial Commit

---
+++ Man's Best Friend/BWViewController.h
@@ -0,0 +1,13 @@
+//
+//  BWViewController.h
+//  Man's Best Friend
+//
+//  Created by Bradley White on 8/14/14.
+//  Copyright (c) 2014 Bradley White. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BWViewController : UIViewController
+
+@end
---
+++ Man's Best Friend/BWViewController.m
@@ -0,0 +1,29 @@
+//
+//  BWViewController.m
+//  Man's Best Friend
+//
+//  Created by Bradley White on 8/14/14.
+//  Copyright (c) 2014 Bradley White. All rights reserved.
+//
+
+#import "BWViewController.h"
+
+@interface BWViewController ()
+
+@end
+
+@implementation BWViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
